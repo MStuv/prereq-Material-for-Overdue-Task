ef7a67784f6b3f027ec628a84b5688b91ea5d48d
MStuv <markstuver@MacBook-Pro.local>
2013-11-10 16:04:01 -0500

    Initial Commit

---
+++ Prereq for Overdue Assignment/MASViewController.m
@@ -0,0 +1,29 @@
+//
+//  MASViewController.m
+//  Prereq for Overdue Assignment
+//
+//  Created by Mark Stuver on 11/10/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import "MASViewController.h"
+
+@interface MASViewController ()
+
+@end
+
+@implementation MASViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Prereq for Overdue Assignment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Prereq for Overdue Assignment
+//
+//  Created by Mark Stuver on 11/10/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MASAppDelegate class]));
+    }
+}
